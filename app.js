d2e600ac141299da15ec90f40c54b656442513ae
saadahsan2023 <124713470+saadahsan2023@users.noreply.github.com>
2023-07-26 03:19:09 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,97 @@
+//    STARTING JAVASCRIPT 🤗;
+//    varible❕✅    container
+
+// var   ✅
+// let   ✅
+// const ✅
+
+// data type❕ ✅
+
+// Number         17 ✅
+// String         " " ✅
+// Boolean        true & false ✅
+// null           null ✅
+// undefined      value does not assign ✅
+// Array          []  squere braket ✅
+// Object         {}  curly  braket ✅
+
+// output dekhny k liyen function❕✅
+
+// document.write() ✅
+// alert() ✅
+// console.log() ✅
+// confirm() ✅
+
+// user sy data leny k liyen❕
+
+// prompt()
+
+// using var   cnt +/
+
+var student = "osama";
+var student = " Ali";
+
+document.write(student);
+
+// var apko allow karta ha k ap 2 ya 2 sy zada ek he name k varible declear kaar skhty hain
+
+//   using  let
+let asadNum = 99;
+
+asadNum = 89;
+
+document.write(asadNum + "<br>");
+
+//   let sy ap varible ki value re assign kar skhty hain
+
+// using const
+
+const gravityOfEarth = 9.8;
+
+document.write(gravityOfEarth);
+
+// const ki value kabi change nh hoti
+
+// data type
+
+// number
+
+let x = 99;
+
+document.write(typeof x);
+
+// String
+let student = "asad";
+document.write(typeof student);
+
+// null
+
+let saad = null;
+
+console.log(saad);
+
+// array
+
+// kisi bhi ek he varible me ek sy zada  value ko store kara dena
+
+let fruit = [" apple ", " graps", "mango", "orange"];
+console.log(fruit);
+
+// object
+
+let person = {
+  name: "ali",
+  fatherName: "shakeel",
+  age: 20,
+  class: 12,
+};
+console.log(person);
+
+let name = " muhammad bin qasim";
+
+alert(name);
+console.log(name);
+
+prompt(" what is your name", " saad");
+
+confirm(" kia ap yahn stay karna chaty hain");
